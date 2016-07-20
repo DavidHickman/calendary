4a5282ab3288a85da109c9c7c6406975e2405209
davidhickman <hickmand@bouldercolorado.gov>
2016-07-20 15:24:03 -0600

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='calendary',
-    version='0.1.3',
+    version='0.1.4',
     description="Python calendar helper.",
     long_description=readme + '\n\n' + history,
     author="David Hickman",
