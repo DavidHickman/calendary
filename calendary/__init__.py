1c4ef83499f95cdc7cb5052a4046c33ca39f2f5c
davidhickman <hickmand@bouldercolorado.gov>
2016-07-20 14:38:57 -0600

    fixing import

--- calendary/__init__.py
+++ calendary/__init__.py
@@ -4,4 +4,4 @@ __author__ = 'David Hickman'
 __email__ = 'davidhickman13@gmail.com'
 __version__ = '0.1.0'
 
-from calendary import Calendary
+from .calendary import Calendary
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='calendary',
-    version='0.1.0',
+    version='0.1.1',
     description="Python calendar helper.",
     long_description=readme + '\n\n' + history,
     author="David Hickman",
