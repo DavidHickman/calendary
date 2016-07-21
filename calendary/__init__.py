1f1a5f5c1757717242388ca8cf36dfe728f086e1
davidhickman <hickmand@bouldercolorado.gov>
2016-07-21 07:42:21 -0600

    updating travis .yml

--- calendary/__init__.py
+++ calendary/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from .calendary import Calendary
 
 __author__ = 'David Hickman'
 __email__ = 'davidhickman13@gmail.com'
 __version__ = '0.1.7'
 
-from .calendary import Calendary
