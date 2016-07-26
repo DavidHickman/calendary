a7682d36e41a87cc58fe4fd3e4185779473f5874
davidhickman <hickmand@bouldercolorado.gov>
2016-07-26 07:46:58 -0600

    Bump version: 0.3.0 → 0.3.1

--- calendary/__init__.py
+++ calendary/__init__.py
@@ -3,5 +3,5 @@ from .calendary import Calendary
 
 __author__ = 'David Hickman'
 __email__ = 'davidhickman13@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='calendary',
-    version='0.3.0',
+    version='0.3.1',
     description="Python calendar helper.",
     long_description=readme + '\n\n' + history,
     author="David Hickman",
